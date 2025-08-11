586b83810a6ac28de541101f786cd8cf353f5a89
Pradeep Bastola <pradeepbastola13@gmail.com>
2025-08-11 19:17:40 -0400

    Create min.js

---
+++ assets/js/min.js
@@ -0,0 +1,7 @@
+var loader = document.getElementById('loading');
+function myFunction() {
+    loader.style.display= 'none';
+}
+function myFunctions(){
+    document.getElementById("date").innerHTML=Date();
+}
